9230ee8a4e049c883386eb4bdd21271c0012ad17
AlexandroMtzG <alex.martinez@absys.com.mx>
2022-03-28 04:45:47 -0600

    initial

---
+++ app/root.tsx
@@ -0,0 +1,139 @@
+import {
+  LiveReload,
+  Outlet,
+  LinksFunction,
+  Links,
+  useCatch,
+  MetaFunction,
+  Meta,
+  Scripts,
+  ScrollRestoration,
+  LoaderFunction,
+  useLoaderData,
+  ActionFunction,
+} from "remix";
+import styles from "./styles/app.css";
+import { useSetupTranslations } from "remix-i18next";
+import { createUserSession, getUserInfo } from "./utils/session.server";
+import { loadRootData, useRootData } from "./utils/data/useRootData";
+import TopBanner from "./components/ui/banners/TopBanner";
+
+export let links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: styles }];
+};
+
+export const meta: MetaFunction = () => {
+  const description = `Learn Remix and laugh at the same time!`;
+  return {
+    charset: "utf-8",
+    description,
+    keywords: "Remix,jokes",
+    "twitter:image": "https://remix-jokes.lol/social.png",
+    "twitter:card": "summary_large_image",
+    "twitter:creator": "@remix_run",
+    "twitter:site": "@remix_run",
+    "twitter:title": "Remix Jokes",
+    "twitter:description": description,
+  };
+};
+
+function Document({ children, title = `Remix: So great, it's funny!` }: { children: React.ReactNode; title?: string }) {
+  const data = useRootData();
+  return (
+    <html lang={data?.locale} className={data?.lightOrDarkMode === "dark" ? "dark" : ""}>
+      <head>
+        <meta charSet="utf-8" />
+        <Meta />
+        <title>{title}</title>
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <Links />
+      </head>
+      <body className="min-h-screen text-gray-800 dark:text-white bg-white dark:bg-slate-900 max-w-full max-h-full">
+        <TopBanner />
+        {children}
+        <Scripts />
+        <LiveReload />
+        <ScrollRestoration />
+
+        <script async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+        <noscript>
+          <img src="https://queue.simpleanalyticscdn.com/noscript.gif" alt="" />
+        </noscript>
+      </body>
+    </html>
+  );
+}
+
+export let loader: LoaderFunction = async ({ request }) => {
+  return loadRootData(request);
+};
+
+export const action: ActionFunction = async ({ request }) => {
+  const userInfo = await getUserInfo(request);
+  const form = await request.formData();
+  const type = form.get("type");
+  const redirect = form.get("redirect")?.toString();
+  if (type === "toggleLightOrDarkMode") {
+    const current = userInfo?.lightOrDarkMode ?? "dark";
+    const lightOrDarkMode = current === "dark" ? "light" : "dark";
+    return createUserSession(
+      {
+        userId: userInfo?.userId ?? "",
+        currentTenantId: userInfo?.currentTenantId ?? "",
+        currentWorkspaceId: userInfo?.currentWorkspaceId ?? "",
+        // locale: userInfo?.currentWorkspaceId ?? "en",
+        lightOrDarkMode,
+      },
+      redirect
+    );
+  }
+  if (type === "setLocale") {
+    return createUserSession(
+      {
+        userId: userInfo?.userId,
+        currentTenantId: userInfo?.currentTenantId,
+        currentWorkspaceId: userInfo?.currentWorkspaceId,
+        lightOrDarkMode: userInfo?.lightOrDarkMode,
+      },
+      redirect
+    );
+  }
+};
+
+export default function App() {
+  let { locale } = useLoaderData<{ locale: string }>();
+  useSetupTranslations(locale);
+  return (
+    <Document>
+      <Outlet />
+    </Document>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  return (
+    <Document title={`${caught.status} ${caught.statusText}`}>
+      <div className="error-container">
+        <h1>
+          {caught.status} {caught.statusText}
+        </h1>
+      </div>
+    </Document>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  return (
+    <Document title="Uh-oh!">
+      <div className="error-container">
+        <h1>App Error</h1>
+        <pre>{error.message}</pre>
+      </div>
+    </Document>
+  );
+}
---
+++ app/routes/index.tsx
@@ -0,0 +1,57 @@
+import Features from "~/components/front/Features";
+import Footer from "~/components/front/Footer";
+import Hero from "~/components/front/Hero";
+import JoinNow from "~/components/front/JoinNow";
+import { json, LoaderFunction, MetaFunction, useCatch, useLoaderData } from "remix";
+import { i18n } from "~/locale/i18n.server";
+import { Language } from "remix-i18next";
+import { getUserInfo } from "~/utils/session.server";
+
+export const meta: MetaFunction = () => ({
+  title: "Remix SaasFrontend",
+});
+
+type LoaderData = {
+  authenticated: boolean;
+  i18n: Record<string, Language>;
+};
+
+export let loader: LoaderFunction = async ({ request }) => {
+  const userInfo = await getUserInfo(request);
+  const data: LoaderData = {
+    authenticated: (userInfo?.userId ?? "").length > 0,
+    i18n: await i18n.getTranslations(request, ["translations"]),
+  };
+  return json(data);
+};
+
+export default function IndexRoute() {
+  return (
+    <div>
+      <div className="relative overflow-hidden bg-white dark:bg-gray-900 text-gray-800 dark:text-slate-200">
+        <Hero />
+        <Features className="relative z-10" />
+        <JoinNow />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  return (
+    <div className="error-container">
+      <h1>{`${caught.status} ${caught.statusText}`}</h1>
+    </div>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  return (
+    <div className="error-container">
+      <h1>App Error</h1>
+    </div>
+  );
+}
