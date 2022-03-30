084eec89ad4304904e6f7cf6cef75787c28fdb4a
AlexandroMtzG <alex.martinez@absys.com.mx>
2022-03-29 23:06:43 -0600

    fetch i18n backend

--- app/locale/i18n.server.ts
+++ app/locale/i18n.server.ts
@@ -1,9 +1,10 @@
-import { RemixI18Next } from "remix-i18next";
-import { FileSystemBackend } from "remix-i18next";
+import { FetchBackend, RemixI18Next } from "remix-i18next";
 // You will need to provide a backend to load your translations, here we use the
 // file system one and tell it where to find the translations.
-const path = require("path");
-let backend = new FileSystemBackend(path.resolve("./public/locales"));
+let backend = new FetchBackend({
+  baseUrl: new URL(process.env.REMIX_SERVER_URL?.toString() ?? ""),
+  pathPattern: "/locales/:locale/:namespace.json",
+});
 
 export let i18n = new RemixI18Next(backend, {
   fallbackLng: "en", // here configure your default (fallback) language
