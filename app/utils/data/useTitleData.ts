090d8e857095db7944cba5794610b2715450b4e5
AlexandroMtzG <alex.martinez@absys.com.mx>
2022-07-01 12:58:33 -0500

    useTitleData temporary fix

--- app/utils/data/useTitleData.ts
+++ app/utils/data/useTitleData.ts
@@ -1,12 +1,16 @@
 import { useMatches } from "remix";
 
 export function useTitleData(): string {
-  const titles = useMatches()
-    .map((match) => match.data)
-    .filter((data) => Boolean(data.title))
-    .map((data) => data.title);
-  if (!titles || titles.length === 0) {
+  try {
+    const titles = useMatches()
+      .map((match) => match.data)
+      .filter((data) => Boolean(data.title))
+      .map((data) => data.title);
+    if (!titles || titles.length === 0) {
+      return "";
+    }
+    return titles[titles.length - 1].split("|")[0].trim() ?? "";
+  } catch {
     return "";
   }
-  return titles[titles.length - 1].split("|")[0].trim() ?? "";
 }
