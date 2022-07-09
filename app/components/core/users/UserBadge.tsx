ebe45929aaa9e7002cbfacaedd2d2621f66c5979
AlexandroMtzG <alex.martinez@absys.com.mx>
2022-07-09 12:40:46 -0500

    UserBadge without hard-coded color

--- app/components/core/users/UserBadge.tsx
+++ app/components/core/users/UserBadge.tsx
@@ -20,16 +20,16 @@ export default function UserBadge({ item, admin, withEmail = true, withAvatar }:
     <>
       {!withAvatar ? (
         <div>
-          {item.firstName} {item.lastName} {withEmail && <span className="italic text-xs font-normal text-gray-500">({item.email})</span>}
+          {item.firstName} {item.lastName} {withEmail && <span className="italic text-xs font-normal opacity-80">({item.email})</span>}
         </div>
       ) : (
         <div className="flex items-center">
           {item.avatar && <UserAvatarBadge avatar={item.avatar} />}
           <div className="ml-3 truncate">
-            <div className="font-medium text-gray-900 truncate">
+            <div className="font-medium truncate">
               {item.firstName} {item.lastName} {admin && <span className="text-xs text-theme-500">({t("shared.admin")})</span>}
             </div>
-            <div className="text-gray-500 truncate">{item.email}</div>
+            <div className="opacity-80 truncate">{item.email}</div>
           </div>
         </div>
       )}
