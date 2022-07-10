6948908e51e3ab4aef4cd15c28ab4d00f8bf1cdb
AlexandroMtzG <alex.martinez@absys.com.mx>
2022-07-10 15:12:41 -0500

    RowActivity check if user is set (may come from the public/ route)

--- app/components/entities/rows/RowActivity.tsx
+++ app/components/entities/rows/RowActivity.tsx
@@ -74,7 +74,7 @@ export default function RowActivity({ entity, items }: Props) {
                             <div>
                               <div className="relative px-1">
                                 <div className="relative">
-                                  <UserAvatarBadge className="h-9 w-9" avatar={appOrAdminData.user.avatar} />
+                                  <UserAvatarBadge className="h-9 w-9" avatar={appOrAdminData.user?.avatar} />
 
                                   <span className="absolute -bottom-0.5 -right-1 bg-gray-50 rounded-tl px-0.5 py-px">
                                     {/* <TagIcon className="h-4 w-4 text-gray-500" aria-hidden="true" /> */}
@@ -124,7 +124,7 @@ export default function RowActivity({ entity, items }: Props) {
                 <div className="flex space-x-3">
                   <div className="flex-shrink-0">
                     <div className="relative px-1">
-                      <UserAvatarBadge className="h-9 w-9" avatar={appOrAdminData.user.avatar} />
+                      <UserAvatarBadge className="h-9 w-9" avatar={appOrAdminData.user?.avatar} />
 
                       <span className="absolute -bottom-0.5 -right-1 bg-gray-50 rounded-tl px-0.5 py-px">
                         <ChatAltIcon className="h-4 w-4 text-gray-400" aria-hidden="true" />
