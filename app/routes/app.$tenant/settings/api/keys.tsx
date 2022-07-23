42b30c3154a7b1bbc9174506b6a1e91acd8b1253
AlexandroMtzG <alex.martinez@absys.com.mx>
2022-07-23 17:27:44 -0500

    fixed tenant api/key and api/logs layout and input date text color

--- app/routes/app.$tenant/settings/api/keys.tsx
+++ app/routes/app.$tenant/settings/api/keys.tsx
@@ -1,10 +1,13 @@
 import { json, LoaderFunction } from "@remix-run/node";
-import { Outlet, useLoaderData } from "@remix-run/react";
+import { Outlet, useLoaderData, useParams } from "@remix-run/react";
+import { useTranslation } from "react-i18next";
 import { DefaultFeatures } from "~/application/dtos/shared/DefaultFeatures";
 import { PlanFeatureUsageDto } from "~/application/dtos/subscriptions/PlanFeatureUsageDto";
 import ApiKeysTable from "~/components/core/apiKeys/ApiKeysTable";
 import CheckPlanFeatureLimit from "~/components/core/settings/subscription/CheckPlanFeatureLimit";
 import InfoBanner from "~/components/ui/banners/InfoBanner";
+import IndexPageLayout from "~/components/ui/layouts/IndexPageLayout";
+import UrlUtils from "~/utils/app/UrlUtils";
 import { useAppData } from "~/utils/data/useAppData";
 import { ApiKeyLogSimple, ApiKeyWithDetails, getAllApiKeyLogsSimple, getApiKeys } from "~/utils/db/apiKeys.db.server";
 import { getPlanFeatureUsage } from "~/utils/services/subscriptionService";
@@ -29,27 +32,43 @@ export let loader: LoaderFunction = async ({ params }) => {
 };
 
 export default function AdminApiKeysRoute() {
+  const { t } = useTranslation();
   const data = useLoaderData<LoaderData>();
   const appData = useAppData();
+  const params = useParams();
   return (
     <>
-      <CheckPlanFeatureLimit item={data.featurePlanUsage} hideContent={false}>
-        <div className="space-y-2">
-          <ApiKeysTable
-            entities={appData.entities}
-            items={data.apiKeys}
-            logs={data.apiKeyLogs}
-            withTenant={false}
-            canCreate={appData.permissions.includes("app.settings.apiKeys.create")}
-          />
-          {data.featurePlanUsage?.enabled && (
-            <InfoBanner title="API usage" text="API calls remaining: ">
-              <b>Remaining {data.featurePlanUsage?.remaining}</b>
-            </InfoBanner>
-          )}
-        </div>
-        <Outlet />
-      </CheckPlanFeatureLimit>
+      <IndexPageLayout
+        replaceTitleWithTabs={true}
+        tabs={[
+          {
+            name: t("models.apiKey.plural"),
+            routePath: UrlUtils.currentTenantUrl(params, "settings/api/keys"),
+          },
+          {
+            name: t("models.apiKey.logs"),
+            routePath: UrlUtils.currentTenantUrl(params, "settings/api/logs"),
+          },
+        ]}
+      >
+        <CheckPlanFeatureLimit item={data.featurePlanUsage} hideContent={false}>
+          <div className="space-y-2">
+            <ApiKeysTable
+              entities={appData.entities}
+              items={data.apiKeys}
+              logs={data.apiKeyLogs}
+              withTenant={false}
+              canCreate={appData.permissions.includes("app.settings.apiKeys.create")}
+            />
+            {data.featurePlanUsage?.enabled && (
+              <InfoBanner title="API usage" text="API calls remaining: ">
+                <b>Remaining {data.featurePlanUsage?.remaining}</b>
+              </InfoBanner>
+            )}
+          </div>
+          <Outlet />
+        </CheckPlanFeatureLimit>
+      </IndexPageLayout>
     </>
   );
 }
